b9cbb3ad4c44ed8b38b322ed60f634caa7300d0b
root <root@ip-88-0-0-28.us-east-2.compute.internal>
2022-04-07 06:59:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1 style='text-align: center; font-size: 64px'>This is k8s Demo site from node.js project</h1>"
+  );
+});
+
+app.get("/user", function(req, res) {
+  res.status(200).json({ name: "appleboy" });
+});
+
+// listen 8080 port
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app; // for testing
